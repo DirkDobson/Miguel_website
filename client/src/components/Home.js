c3250326618fbd242bb89d31031ab2193ae64a4a
Dirk Dobson <dirkvancouver@live.com>
2018-10-28 22:26:29 -0600

    photo add

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import React, { Component } from 'react'
+import { Header } from 'semantic-ui-react'
+import Miguel_photo0 from '../Images/Miguel_photo0.jpg'
 
 class Home extends Component {
   render() {
     return (
+      <>
       <Header as="h1" textAlign="center">Home Component</Header>
+      <img src={Miguel_photo0} alt="Migule playing Guitar" />
+      </>
     );
   }
 }
