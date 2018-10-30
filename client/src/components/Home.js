c334e4c82d0f00cf60a15cddc28dd9c35f6df367
Dirk Dobson <dirkvancouver@live.com>
2018-10-29 21:29:40 -0600

    background color

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
 import { Header } from 'semantic-ui-react'
 import Miguel_photo0 from '../Images/Miguel_photo0.jpg'
+import styled from 'styled-components'
 
 class Home extends Component {
   render() {
     return (
-      <>
+      <Body>
       <Header as="h1" textAlign="center">Home Component</Header>
-      <img src={Miguel_photo0} alt="Migule playing Guitar" />
-      </>
+      <Hero src={Miguel_photo0} alt="Migule playing Guitar" />
+      </Body>
     );
   }
 }
+const Hero = styled.img`
+  display: flex;
+  justify-content: center;
+`
+const Body = styled.div`
+ background-color: black;
+` 
+export default Home
 
-export default Home;
 
