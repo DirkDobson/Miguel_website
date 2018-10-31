9d1aa1c1a7a3f5973d952cd505b4361578d642bb
Dirk Dobson <dirkvancouver@live.com>
2018-10-31 16:58:16 -0600

    formating

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -12,17 +12,29 @@ class Home extends Component {
       {/* <Header as="h1" textAlign="center">Home Component</Header> */}
       <Hero src={Miguel_photo0} alt="Migule playing Guitar" />
       <NavBar />
-      <div>
+      <Par>Things That Miguel Does</Par>
+      <Divid>
         <Description>
          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <br />
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+         totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+         dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, 
+         sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+          Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+          sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+           voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit 
+           laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit 
+           qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum 
+           fugiat quo voluptas nulla pariatur?
         </Description>
-        <video width="400" controls>
+        <Miguel width="400" controls>
           <source src={LimeRush} type="video/mp4" />
-        </video>
-      </div>
+        </Miguel>
+      </Divid>
       </Body>
     );
   }
@@ -42,9 +54,27 @@ const Description = styled.section`
   color: white;
   display: block;
   margin-left: auto;
-  margin-right: auto;
-  width: 800px;
+  margin-right: 100px;
+  margin-top: 45px;
+  width: 500px;
   text-align: center;
+`
+
+const Divid = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`
+
+const Miguel = styled.video`
+ margin-right: 300px;
+`
+
+const Par = styled.p`
+color: white;
+display: flex;
+justify-content: center;
+font-size: 1.2em;
 `
 export default Home
 
