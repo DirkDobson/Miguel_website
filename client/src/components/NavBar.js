61193b2f61422a65f9658b9a4077e2b33fe884c7
Dirk Dobson <dirkvancouver@live.com>
2018-11-15 22:10:00 -0700

    changed nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,75 +2,44 @@ import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { handleLogout } from '../reducers/user';
-import styled from 'styled-components'
 
 class NavBar extends Component {
   rightNavs = () => {
     const { user, dispatch, history } = this.props
   }
 
-  //   if (user.id) {
-  //     return (
-  //       <FMenu.FMenu position='right'>
-  //         <FMenu.Item
-  //           name='Logout'
-  //           onClick={() => dispatch(handleLogout(history))}
-  //         />
-  //       </FMenu.FMenu>
-  //     );
-  //   }
-  //   return (
-  //     <FMenu.FMenu position="right">
-  //       <Link to="/register">
-  //         <FMenu.Item name="Register" />
-  //       </Link>
-  //       <Link to="/login">
-  //         <FMenu.Item name="Login" />
-  //       </Link>
-  //     </FMenu.FMenu>
-  //   );
-  // }
 
   render() {
     return (
-      <Place>
-        <Menu secondary inverted >
-          <Link to="/home" >
-            <Menu.Item name='Home' />
-          </Link>
-          <Link to="/studio_work">
-            <Menu.Item name='Studio Work' />
-          </Link>
-          <Link to="/photos">
-            <Menu.Item name='Photos' />
-          </Link>
-          <Link to="videos">
-            <Menu.Item name='Videos' />
-          </Link>
-          <Link to="music">
-            <Menu.Item name='Music' />
-          </Link>
-          <Link to="demos">
-            <Menu.Item name='Demos' />
-          </Link>
-          <Link to="/downloads">
-            <Menu.Item name='Downloads' />
-          </Link>
-          <Link to="contact">
-            <Menu.Item name='Contact' />
-          </Link>
-        </Menu>
-      </Place>
+      <Menu secondary color={'blue'} inverted >
+        <Link to="/" >
+          <Menu.Item name='Home' />
+        </Link>
+        <Link to="/studio_work">
+          <Menu.Item name='Studio Work' />
+        </Link>
+        <Link to="/photos">
+          <Menu.Item name='Photos' />
+        </Link>
+        <Link to="videos">
+          <Menu.Item name='Videos' />
+        </Link>
+        <Link to="music">
+          <Menu.Item name='Music' />
+        </Link>
+        <Link to="demos">
+          <Menu.Item name='Demos' />
+        </Link>
+        <Link to="/downloads">
+          <Menu.Item name='Downloads' />
+        </Link>
+        <Link to="contact">
+          <Menu.Item name='Contact' />
+        </Link>
+      </Menu>
     );
   }
 }
-
-const Place = styled.div`
-  display: flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
-`
 
 
 
