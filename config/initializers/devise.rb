33205bc7ec50f9f3667ddf5513fef0f7b1b2d9a0
Dirk Dobson <dirkvancouver@live.com>
2018-12-22 13:02:51 -0700

    remove files

--- config/initializers/devise.rb
+++
@@ -1,9 +0,0 @@
-Devise.setup do |config|
-  if Rails.env.development?
-    key = SecureRandom.hex
-  else 
-    key = ENV['DEVISE_SECRET_KEY'] || SecureRandom.hex
-  end
-
-  config.secret_key = key
-end
